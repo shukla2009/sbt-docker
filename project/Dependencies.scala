dd8a31b3af882cdbe182de7aec09f1b6a72e5ecc
Rahul <shukla2009@gmail.com>
2017-10-20 15:19:56 +0530

    adding more dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,6 +8,7 @@ object Dependencies {
     val spark = "2.2.0"
     val akka = "2.5.3"
     val uap = "0.1.2"
+    val cache = "0.9.4"
   }
 
   lazy val library = new {
@@ -21,16 +22,31 @@ object Dependencies {
     lazy val sql = "org.apache.spark" %% "spark-sql" % version.spark //% "provided"
     lazy val kinesis = "org.apache.spark" %% "spark-streaming-kinesis-asl" % version.spark //% "provided"
     lazy val cassandra = "com.datastax.spark" %% "spark-cassandra-connector" % "2.0.3" //% "provided"
+  }
 
+  lazy val akka = new {
+    lazy val actor = "com.typesafe.akka" %% "akka-actor" % version.akka
+    lazy val cluster = "com.typesafe.akka" %% "akka-cluster" % version.akka
+    lazy val clusterMetrics = "com.typesafe.akka" % "akka-cluster-metrics_2.11" % version.akka
+    lazy val clusterTool = "com.typesafe.akka" %% "akka-cluster-tools" % version.akka
+    lazy val testkit = "com.typesafe.akka" %% "akka-testkit" % version.akka % "test"
   }
 
   lazy val config = "com.typesafe" % "config" % "1.3.1"
+  lazy val jdbc = "org.xerial" % "sqlite-jdbc" % "3.8.11.2"
+  lazy val cache = new {
+    lazy val scala = "com.github.cb372" %% "scalacache-core" % version.cache
+    lazy val eh = "com.github.cb372" %% "scalacache-ehcache" % version.cache
+    lazy val redis = "com.github.cb372" %% "scalacache-redis" % version.cache
+  }
 
   val dep: Seq[ModuleID] = Seq(
     spark.core,
     spark.sql,
     spark.kinesis,
     spark.cassandra,
+    cache.scala,
+    cache.redis,
     config
   )
 }
